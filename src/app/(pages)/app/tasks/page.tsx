f2eeefcdc86fb8dace38b6c5955d9640d9cb4c67
Akhil S R <98821434+AkLuffy07@users.noreply.github.com>
2025-10-14 20:53:08 +0530

    added google card to task page

--- src/app/(pages)/app/tasks/page.tsx
+++ src/app/(pages)/app/tasks/page.tsx
@@ -2,6 +2,10 @@ export default function TasksPage() {
     return (
         <div>
             <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Tasks</h1>
+            <a href="https://google.com" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Google Website</h5>
+                <p class="font-normal text-gray-700 dark:text-gray-400">Browser where you can search your desired topics.....</p>
+            </a>
         </div>
     );
 }
